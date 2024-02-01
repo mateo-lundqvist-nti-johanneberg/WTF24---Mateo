cbb60776d40f8416662e1dfd4c259683d363415f
mateo-lundqvist-nti-johanneberg <119052302+mateo-lundqvist-nti-johanneberg@users.noreply.github.com>
2024-02-01 16:13:47 +0100

    idag klart

--- src/app.rb
+++ src/app.rb
@@ -2,13 +2,14 @@ class App < Sinatra::Base
 
     def db
         if @db == nil
-            @db = SQLite3::Database.new('./db/db.sqlite')
+            @db = SQLite3::Database.new('./db/db.db')
             @db.results_as_hash = true
         end
         return @db
     end
 
     get '/' do
+        @items = db.execute("SELECT * FROM items")
         erb :index
     end
 
